b5a004c2974963b40ede8d91be8f50a97c83b1d0
Jules Blom <julesblom@gmail.com>
2017-07-12 17:06:08 +0200

    Zoom, Gemeente borders

--- main.js
+++ main.js
@@ -1,35 +1,62 @@
-var margin = {top: 40, right: 40, bottom: 40, left: 40};
+/* TODO
+-Make zoomable like
+https://bl.ocks.org/mbostock/4699541
+or https://bl.ocks.org/mbostock/9656675
+-Province borders
 
-var svg = d3.select("#map"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height"),
-    format = d3.format(",d");
-
-var svgLegend = d3.select("#legend");
+-https://bl.ocks.org/iamkevinv/0a24e9126cd2fa6b283c6f2d774b69a2
+*/
+var width = 960,
+    height = 700,
+    format = d3.format(",d")
+    active = d3.select(null);
 
 var projection = d3.geoAlbers() 
     .center([5.5, 52.05])
-    .parallels([50, 53])
+    // .parallels([50, 53])
     .rotate(120)
     .scale(12000)
     .translate([width / 2, height / 2]);
 
+var zoom = d3.zoom()
+    .scaleExtent([1, 8])
+    .on("zoom", zoomed);
+
 var path = d3.geoPath()
     .projection(projection);
+
+var svg = d3.select("#map")
+    .attr("width", width)
+    .attr("height", height)
+    .on("click", stopped, true);
+
+svg.append("rect")
+    .attr("class", "background")
+    .attr("width", width)
+    .attr("height", height)
+    .on("click", reset);
+
+var g = svg.append("g");
+
+svg.call(zoom);
+
+var svgLegend = d3.select("#legend");
 
 var color = d3.scaleThreshold()
     .domain([0, 100, 500, 750, 1500, 2500, 7500, 12500])
     .range(d3.schemeGnBu[9]);
 
-var x = d3.scaleLinear()
+
+/* LEGEND */
+var x = d3.scaleSqrt()
     .domain([0, 12500])
     .rangeRound([10, 890]);
 
-var g = svgLegend.append("g")
+var gLegend = svgLegend.append("g")
     .attr("class", "key")
     .attr("transform", "translate(0,40)");
 
-g.selectAll("rect")
+gLegend.selectAll("rect")
   .data(color.range().map(function(d) {
       d = color.invertExtent(d);
       if (d[0] == null) d[0] = x.domain()[0];
@@ -42,7 +69,8 @@ g.selectAll("rect")
     .attr("width", function(d) { return x(d[1]) - x(d[0]); })
     .attr("fill", function(d) { return color(d[0]); });
 
-g.append("text")
+// Draw legend text
+gLegend.append("text")
     .attr("class", "caption")
     .attr("x", x.range()[0])
     .attr("y", -6)
@@ -51,17 +79,18 @@ g.append("text")
     .attr("font-weight", "bold")
     .text("Inwoners per vierkante kilometer");
 
-g.call(d3.axisBottom(x)
+// Draw axis tick marks
+gLegend.call(d3.axisBottom(x)
     .tickSize(13)
     .tickValues(color.domain()))
   .select(".domain")
     .remove();
 
-// Select import or export with radio button
+// Toggle voronoi with radio button
 var inputElems = d3.selectAll("input")
   .on("change", function() {
-    if (this.value === "Hide") {d3.select(".voronoi").style("opacity", 0);}
-    else if (this.value === "Show") {d3.select(".voronoi").style("opacity", 0.55)}
+    if (this.value === "Hide") { d3.select(".voronoi").style("opacity", 0); }
+    else if (this.value === "Show") { d3.select(".voronoi").style("opacity", 1); }
 })
 
 d3.queue()
@@ -74,40 +103,44 @@ function ready(error, wijken, stations, spoor) {
   if (error) throw error;
 
   // Draw the map
-  svg.selectAll(".wijk")
+  g.selectAll(".wijk")
       .data(topojson.feature(wijken, wijken.objects.wijk).features)
     .enter().insert("path")
       .attr("class", "wijk")
       .attr("fill", function(d) { return color(d.properties.BEV_DICHTH); }) //d3.schemeOrRd[9][d.properties.AANT_INW]
       .attr("d", path)
+      .on("click", clicked)
     .append("title")
       .text(function(d) { return (
         d.properties.GM_NAAM + " "
         + ((d.properties.WK_NAAM !== null) ? d.properties.WK_NAAM : "")
-        + "\nGemiddelde afstand tot station: " + d.properties.AF_TREINST + " km"
-        + ((d.properties.BEV_DICHTH !== -99999998 || -99999997) ? "\nBevolkingsdichtheid: " + format(d.properties.BEV_DICHTH) : "")
+        + ((d.properties.AF_TREINST !== -99999998) ? "\nGem. afstand tot station: " + d.properties.AF_TREINST + " km" : "")
+        + ((d.properties.BEV_DICHTH !== -99999998) ? "\nBev. dichtheid: " + format(d.properties.BEV_DICHTH) : "")
         + ((d.properties.AANT_INW !== -99999998) ? "\nAantal Inwoners: " + format(d.properties.AANT_INW) : "")
         );
       });
-// ((d.properties.AANT_INW !== -99999998) ? "\nAantal Inwoner: " + format(d.properties.AANT_INW) : " ");
 
   // Draw borders
-  svg.append("path")
+  g.append("path")
       .attr("class", "wijk-borders")
       .attr("d", path(topojson.mesh(wijken, wijken.objects.wijk, function(a, b) { return a !== b; })));
 
+  // Draw borders
+  g.append("path")
+      .attr("class", "gemeente-borders")
+      .attr("d", path(topojson.mesh(wijken, wijken.objects.wijk, function(a, b) { return a.properties.GM_NAAM !== b.properties.GM_NAAM; })));
+
   // Draw the tracks
-  svg.selectAll(".spoor")
+  g.selectAll(".spoor")
       .data(spoor.features)
     .enter().insert("path")
       .attr("class", "spoor")
       .attr("d", path);
 
   // Draw the points for the stations
-  svg.selectAll(".station")
+  g.selectAll(".station")
     .data(stations)
     .enter().append("circle")
-      .attr("r", 1.6)
       .attr("class", "station")
       .attr("transform", function(d) { return "translate(" + projection([d["0"], d["1"]]) + ")"; })
       .append("title")
@@ -118,7 +151,7 @@ function ready(error, wijken, stations, spoor) {
                   .polygons(stations.map(projection));
 
   // Draw the Voronoi cells
-  svg.append("path")
+  g.append("path")
     .datum(polygons)
     .attr("class", "voronoi")
     .attr("d", function(d) {
@@ -128,3 +161,42 @@ function ready(error, wijken, stations, spoor) {
         .join("ZM") + "Z";
     });
 };
+
+function clicked(d) {
+  if (active.node() === this) return reset();
+  active.classed("active", false);
+  active = d3.select(this).classed("active", true);
+
+  var bounds = path.bounds(d),
+      dx = bounds[1][0] - bounds[0][0],
+      dy = bounds[1][1] - bounds[0][1],
+      x = (bounds[0][0] + bounds[1][0]) / 2,
+      y = (bounds[0][1] + bounds[1][1]) / 2,
+      scale = Math.max(1, Math.min(8, 0.9 / Math.max(dx / width, dy / height))),
+      translate = [width / 2 - scale * x, height / 2 - scale * y];
+
+  svg.transition()
+      .duration(750)
+      .call( zoom.transform, d3.zoomIdentity.translate(translate[0],translate[1]).scale(scale) );
+}
+
+function reset() {
+  active.classed("active", false);
+  active = d3.select(null);
+
+  svg.transition()
+      .duration(750)
+      .call( zoom.transform, d3.zoomIdentity );
+}
+
+function zoomed() {
+  g.style("stroke-width", 1.5 / d3.event.transform.k + "px");
+  g.attr("transform", d3.event.transform);
+}
+
+// If the drag behavior prevents the default click,
+// also stop propagation so we don’t click-to-zoom.
+function stopped() {
+  if (d3.event.defaultPrevented) d3.event.stopPropagation();
+}
+
